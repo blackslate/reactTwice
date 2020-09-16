438bf5ae7a93748ebc863d3bada78c01301d1dd2
James Newton <james.lexogram@gmail.com>
2020-09-16 13:21:07 +0300

    Barebones demo that shows constructor and render called twice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let instance = 0
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.startTime = + new Date()
+    this.instance = ++instance
+    this.state = {}
+    this.imageError = this.imageError.bind(this)
+
+    console.log("App constructor (", this.instance, ")", this.getTime())
+  }
+
+
+  getTime() {
+    return + new Date() - this.startTime 
+  }
+
+
+  runTimeOutMethod() {
+    console.log("runTimeOutMethod started (", this.instance, ")", this.getTime())
+    this.setState({ time: true })
+    console.log("runTimeOutMethod complete (", this.instance, ")",  this.getTime())
+  }
+
+
+  imageError(event) {
+    console.log("DOM element added (", this.instance, ")", this.getTime())
+  }
+
+
+  render() {
+    console.log(
+      "render called (", this.instance, ")"
+    ,  this.getTime()
+    , this.state
+    , this.props
+    )
+
+    return (
+      <div className="App">
+       <img src="" alt="no src" onError={this.imageError} />
+      </div>
+    );
+  }
+
+
+  componentDidMount() {
+    console.log("componentDidMount started (", this.instance, ")", this.getTime())
+    this.setState({ mounted: true })
+    setTimeout(this.runTimeOutMethod.bind(this), 0)
+    console.log("componentDidMount complete (", this.instance, ")", this.getTime())
+  }
 }
-
-export default App;
